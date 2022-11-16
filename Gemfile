d13a770b6d8fc827656216e230d6b0cef6997105
nafti ladhiri <ladhiri.nafti@gmail.com>
2022-11-16 15:28:00 +0100

    done

--- Gemfile
+++ Gemfile
@@ -12,6 +12,8 @@ gem "sprockets-rails"
 # Use sqlite3 as the database for Active Record
 gem "sqlite3", "~> 1.4"
 
+gem "faker"
+
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", "~> 5.0"
 
@@ -63,3 +65,4 @@ end
 
 gem 'rspec-rails', group: [ :test ]
 gem 'rails-controller-testing', group: [ :test ]
+gem "simple_form", github: "heartcombo/simple_form"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+CATEGORY = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+puts 'Creating 5 fake restaurants...'
+5.times do
+    Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    category: CATEGORY.sample
+  )
+end
+puts 'Finished!'
